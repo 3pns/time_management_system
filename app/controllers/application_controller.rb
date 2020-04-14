63e4e3c604d3de355586b531acf19a7a06a046e1
Samuel BOHN <bohn.sjl@gmail.com>
2020-04-15 02:15:38 +0800

    overidnng secret_key_base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::API
   respond_to :json
   responder :my_application
 
+  def secret_key_base 
+    ENV["SECRET_KEY_BASE"] 
+  end 
 end
--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,5 +1,5 @@
 configatron.configure_from_hash(
   recaptcha_enabled: ENV['RECAPTCHA_ENABLED'],
   front_end_base_url: ENV['FRONT_END_BASE_URL'],
-  from_email: ENV['FROM_EMAIL']
+  from_email: ENV['MAILER_SENDER']
 )
